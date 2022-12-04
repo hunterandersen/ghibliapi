0a30ea7cd837e6dadad3c0dcdccd57f5ef94553c
Clayton Andersen <tunaman65@gmail.com>
2022-12-04 15:20:20 -0800

    added support for firebase deployment model

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require('./src/app')
-const port = process.env.PORT || 3000
+const functions = require("firebase-functions");
+//const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-  console.log('JSONPlaceholder listening on http://localhost:' + port)
-})
+// app.listen(port, () => {
+//   console.log('JSONPlaceholder listening on http://localhost:' + port)
+// })
+
+exports.app = functions.https.onRequest(app);
