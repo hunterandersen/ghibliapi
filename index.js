36f3f763ea1b6b046fcd188ebfa13eaa61732ed6
Hunter Andersen <andersen.hunter42@gmail.com>
2022-12-04 18:37:58 -0800

    Configured Firebase deployment. Replaced all URLs site-wide to match new Firebase domain

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const app = require('./src/app')
+const functions = require("firebase-functions");
+const server = require('./src/app');
+const cors = require('cors')({"origin":true});
+server.use(cors);
+/*
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log('JSONPlaceholder listening on http://localhost:' + port)
-})
+});
+*/
+
+exports.app = functions.https.onRequest(server);
